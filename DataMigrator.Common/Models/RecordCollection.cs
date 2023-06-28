79b6b190f59e07cb184227dc579ab6b56c9be997
Matt <gordon-matt@hotmail.com>
2023-06-28 09:39:11 +0930

    Bug fix for some columns not being renamed (and causing SqlBulkCopy to throw error)

--- DataMigrator.Common/Models/RecordCollection.cs
+++ DataMigrator.Common/Models/RecordCollection.cs
@@ -7,7 +7,7 @@ public class RecordCollection : List<Record>
     public void ReMapFields(IEnumerable<FieldMapping> mappings)
     {
         // Get Fields To Remap (Quicker than passing all items in job.FieldMappings)
-        var fieldToReMap = mappings.Where(x => !x.DestinationField.Name.Equals(x.SourceField.Name, StringComparison.InvariantCultureIgnoreCase));
+        var fieldToReMap = mappings.Where(x => !x.DestinationField.Name.Equals(x.SourceField.Name));
 
         var cachedScripts = mappings.Where(x => !string.IsNullOrEmpty(x.TransformScript))
             .Select(x => (x.SourceField.Name, CSScript.Evaluator.LoadMethod<ITransform>(x.TransformScript)))
